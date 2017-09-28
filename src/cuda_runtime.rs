be966b938fb15389f59429a64add5cfc035815dd
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-09-28 13:13:53 +0900

    add cudaMemcpy

--- src/cuda_runtime.rs
+++ src/cuda_runtime.rs
@@ -1,8 +1,14 @@
 use {c_void, size_t};
 use cudaError;
+use cudaMemcpyKind;
 
 #[link(name = "cudart")]
 extern "C" {
     pub fn cudaMalloc(devPtr: *mut *mut c_void, size: size_t) -> cudaError;
     pub fn cudaFree(devPtr: *mut c_void) -> cudaError;
+    pub fn cudaMemcpy(dst: *mut c_void,
+                      src: *const c_void,
+                      count: size_t,
+                      kind: cudaMemcpyKind)
+                      -> cudaError;
 }
