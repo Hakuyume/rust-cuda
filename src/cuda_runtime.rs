cbb8404ef634f527d1f9139d52046be9a251719e
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-09-28 12:50:08 +0900

    cudaError_t -> cudaError

--- src/cuda_runtime.rs
+++ src/cuda_runtime.rs
@@ -1,8 +1,8 @@
 use {c_void, size_t};
-use cudaError_t;
+use cudaError;
 
 #[link(name = "cudart")]
 extern "C" {
-    pub fn cudaMalloc(devPtr: *mut *mut c_void, size: size_t) -> cudaError_t;
-    pub fn cudaFree(devPtr: *mut c_void) -> cudaError_t;
+    pub fn cudaMalloc(devPtr: *mut *mut c_void, size: size_t) -> cudaError;
+    pub fn cudaFree(devPtr: *mut c_void) -> cudaError;
 }
--- src/driver_types.rs
+++ src/driver_types.rs
@@ -1,7 +1,7 @@
 #![allow(non_camel_case_types)]
 
 #[repr(C)]
-pub enum cudaError_t {
+pub enum cudaError {
     cudaSuccess = 0,
     cudaErrorMemoryAllocation = 2,
     cudaErrorInitializationError = 3,
