c6d60b2525c3131317e366363d1b0c43a40cd76a
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-09-30 12:44:44 +0900

    Revert "impl for mut ref"
    This reverts commit a29f938a10ccaefbc92a39b7c5ae47c4538e43bb.

--- cuda/src/memory/memcpy.rs
+++ cuda/src/memory/memcpy.rs
@@ -7,15 +7,15 @@ use Result;
 use super::Slice;
 
 pub trait MemcpyFrom<S: ?Sized> {
-    fn memcpy_from(self, src: &S) -> Result<()>;
+    fn memcpy_from(&mut self, src: &S) -> Result<()>;
 }
 
-pub fn memcpy<S: ?Sized, D: MemcpyFrom<S>>(dst: D, src: &S) -> Result<()> {
+pub fn memcpy<S: ?Sized, D: ?Sized + MemcpyFrom<S>>(dst: &mut D, src: &S) -> Result<()> {
     dst.memcpy_from(src)
 }
 
-impl<'a, T> MemcpyFrom<[T]> for &'a mut Slice<T> {
-    fn memcpy_from(self, src: &[T]) -> Result<()> {
+impl<T> MemcpyFrom<[T]> for Slice<T> {
+    fn memcpy_from(&mut self, src: &[T]) -> Result<()> {
         assert_eq!(src.len(), self.len());
         unsafe {
             try_call!(cuda_sys::cudaMemcpy(self.as_mut_ptr() as *mut c_void,
@@ -27,8 +27,8 @@ impl<'a, T> MemcpyFrom<[T]> for &'a mut Slice<T> {
     }
 }
 
-impl<'a, T> MemcpyFrom<Slice<T>> for &'a mut [T] {
-    fn memcpy_from(self, src: &Slice<T>) -> Result<()> {
+impl<T> MemcpyFrom<Slice<T>> for [T] {
+    fn memcpy_from(&mut self, src: &Slice<T>) -> Result<()> {
         assert_eq!(src.len(), self.len());
         unsafe {
             try_call!(cuda_sys::cudaMemcpy(self.as_mut_ptr() as *mut c_void,
