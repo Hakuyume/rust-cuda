e3410051c3d5c744e40eaca4bee2a32dc4399e9e
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-10-01 01:29:54 +0900

    use status info

--- cudnn/src/convolution/fwd_algo_perf.rs
+++ cudnn/src/convolution/fwd_algo_perf.rs
@@ -1,10 +1,14 @@
 use cudnn_sys;
 
+use error;
+use Result;
+
 use super::FwdAlgo;
 
 #[derive(Debug)]
 pub struct FwdAlgoPerf {
     pub algo: FwdAlgo,
+    pub status: Result<()>,
     pub time: f64,
     pub memory: usize,
 }
@@ -13,6 +17,7 @@ impl FwdAlgoPerf {
     pub fn from_raw(fwd_algo_perf: cudnn_sys::cudnnConvolutionFwdAlgoPerf) -> FwdAlgoPerf {
         FwdAlgoPerf {
             algo: FwdAlgo::from_raw(fwd_algo_perf.algo),
+            status: error::wrap_status(fwd_algo_perf.status),
             time: fwd_algo_perf.time as f64,
             memory: fwd_algo_perf.memory as usize,
         }
