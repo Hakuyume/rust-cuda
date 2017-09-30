73c250b1a9ae330dc5469b650746a73e86b00243
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-09-30 18:44:09 +0900

    impl as_raw

--- cudnn/src/softmax.rs
+++ cudnn/src/softmax.rs
@@ -14,9 +14,28 @@ pub enum Algorithm {
     Log,
 }
 
+impl Algorithm {
+    pub fn as_raw(self) -> cudnn_sys::cudnnSoftmaxAlgorithm {
+        match self {
+            Algorithm::Fast => cudnn_sys::cudnnSoftmaxAlgorithm::CUDNN_SOFTMAX_FAST,
+            Algorithm::Accurate => cudnn_sys::cudnnSoftmaxAlgorithm::CUDNN_SOFTMAX_ACCURATE,
+            Algorithm::Log => cudnn_sys::cudnnSoftmaxAlgorithm::CUDNN_SOFTMAX_LOG,
+        }
+    }
+}
+
 pub enum Mode {
     Instance,
     Channel,
+}
+
+impl Mode {
+    pub fn as_raw(self) -> cudnn_sys::cudnnSoftmaxMode {
+        match self {
+            Mode::Instance => cudnn_sys::cudnnSoftmaxMode::CUDNN_SOFTMAX_MODE_INSTANCE,
+            Mode::Channel => cudnn_sys::cudnnSoftmaxMode::CUDNN_SOFTMAX_MODE_CHANNEL,
+        }
+    }
 }
 
 pub fn forward<T: scalar::Float>(context: &context::Context,
@@ -32,22 +51,11 @@ pub fn forward<T: scalar::Float>(context: &context::Context,
     assert_eq!(x.len(), x_desc.len());
     assert_eq!(y.len(), y_desc.len());
 
-    let algo = match algo {
-        Algorithm::Fast => cudnn_sys::cudnnSoftmaxAlgorithm::CUDNN_SOFTMAX_FAST,
-        Algorithm::Accurate => cudnn_sys::cudnnSoftmaxAlgorithm::CUDNN_SOFTMAX_ACCURATE,
-        Algorithm::Log => cudnn_sys::cudnnSoftmaxAlgorithm::CUDNN_SOFTMAX_LOG,
-    };
-    let mode = match mode {
-        Mode::Instance => cudnn_sys::cudnnSoftmaxMode::CUDNN_SOFTMAX_MODE_INSTANCE,
-        Mode::Channel => cudnn_sys::cudnnSoftmaxMode::CUDNN_SOFTMAX_MODE_CHANNEL,
-    };
-
     let params: &[T::Scale] = &[alpha.into(), beta.into()];
-
     unsafe {
         try_call!(cudnn_sys::cudnnSoftmaxForward(context.as_raw(),
-                                                 algo,
-                                                 mode,
+                                                 algo.as_raw(),
+                                                 mode.as_raw(),
                                                  &params[0] as *const T::Scale as *const c_void,
                                                  x_desc.as_raw(),
                                                  x.as_ptr() as *const c_void,
