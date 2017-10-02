bbdeda81af0030bfc4dab39f5601f9d79d780fc0
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-10-02 19:56:34 +0900

    use TryFrom

--- cuda/src/error.rs
+++ cuda/src/error.rs
@@ -1,9 +1,10 @@
-use std;
+use std::error;
+use std::ffi;
+use std::fmt;
+use std::result;
 
 use cuda_sys;
 use cuda_sys::cudaError;
-
-use Result;
 
 #[derive(Clone, Copy, Debug)]
 pub enum Error {
@@ -13,38 +14,57 @@ pub enum Error {
     Unknown,
 }
 
-impl std::error::Error for Error {
-    fn description(&self) -> &str {
-        let err = match *self {
+pub trait TryFrom<T>: Sized {
+    type Error;
+    fn try_from(T) -> result::Result<Self, Self::Error>;
+}
+
+impl TryFrom<cudaError> for Error {
+    type Error = ();
+    fn try_from(value: cudaError) -> result::Result<Error, ()> {
+        match value {
+            cudaError::cudaSuccess => Err(()),
+            cudaError::cudaErrorMemoryAllocation => Ok(Error::MemoryAllocation),
+            cudaError::cudaErrorInitializationError => Ok(Error::InitializationError),
+            cudaError::cudaErrorInvalidDevicePointer => Ok(Error::InvalidDevicePointer),
+            cudaError::cudaErrorUnknown => Ok(Error::Unknown),
+        }
+    }
+}
+
+impl Into<cudaError> for Error {
+    fn into(self) -> cudaError {
+        match self {
             Error::MemoryAllocation => cudaError::cudaErrorMemoryAllocation,
             Error::InitializationError => cudaError::cudaErrorInitializationError,
             Error::InvalidDevicePointer => cudaError::cudaErrorInvalidDevicePointer,
             Error::Unknown => cudaError::cudaErrorUnknown,
-        };
+        }
+    }
+}
+
+impl error::Error for Error {
+    fn description(&self) -> &str {
         unsafe {
-            let ptr = cuda_sys::cudaGetErrorString(err);
-            let c_str = std::ffi::CStr::from_ptr(ptr);
+            let ptr = cuda_sys::cudaGetErrorString(Error::into(*self));
+            let c_str = ffi::CStr::from_ptr(ptr);
             c_str.to_str().unwrap_or("[Non UTF8 description]")
         }
     }
 }
 
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{:?}", self)
     }
 }
 
-pub fn wrap_error(err: cudaError) -> Result<()> {
-    match err {
-        cudaError::cudaSuccess => Ok(()),
-        cudaError::cudaErrorMemoryAllocation => Err(Error::MemoryAllocation),
-        cudaError::cudaErrorInitializationError => Err(Error::InitializationError),
-        cudaError::cudaErrorInvalidDevicePointer => Err(Error::InvalidDevicePointer),
-        cudaError::cudaErrorUnknown => Err(Error::Unknown),
-    }
-}
-
 macro_rules! try_call {
-    ($call:expr) => {try!(::error::wrap_error($call))};
+    ($call:expr) => {{
+        use $crate::error::TryFrom;
+        try!(match $crate::Error::try_from($call) {
+            Ok(err) => Err(err),
+            Err(_) => Ok(()),
+        })
+    }};
 }
