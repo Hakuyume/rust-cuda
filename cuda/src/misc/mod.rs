7b3a0f07eb7a1855a08c950e7f6334ac99922552
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-10-03 21:23:21 +0900

    fix API

--- cuda/src/misc/mod.rs
+++ cuda/src/misc/mod.rs
@@ -1,3 +1,5 @@
+use std::ptr;
+
 use cuda_sys;
 use cuda_sys::{c_void, size_t};
 
@@ -11,13 +13,16 @@ pub unsafe fn launch_kernel<T: ?Sized>(func: *const T,
                                        block_dim: Dim3,
                                        args: &mut [*mut c_void],
                                        shared_mem: usize,
-                                       stream: &mut stream::Stream)
+                                       stream: Option<&mut stream::Stream>)
                                        -> Result<()> {
     try_call!(cuda_sys::cudaLaunchKernel(func as *const c_void,
                                          grid_dim.into(),
                                          block_dim.into(),
                                          args.as_mut_ptr(),
                                          shared_mem as size_t,
-                                         stream.as_mut_ptr()));
+                                         match stream {
+                                             Some(stream) => stream.as_mut_ptr(),
+                                             None => ptr::null_mut(),
+                                         }));
     Ok(())
 }
--- cuda/src/stream/mod.rs
+++ cuda/src/stream/mod.rs
@@ -1,21 +1,15 @@
-use std::ptr;
-
 use cuda_sys;
 
 pub struct Stream {
-    stream: Option<cuda_sys::cudaStream>,
+    stream: cuda_sys::cudaStream,
 }
 
 impl Stream {
-    pub fn default() -> Stream {
-        Stream { stream: None }
-    }
-
     pub fn as_ptr(&self) -> cuda_sys::cudaStream {
-        self.stream.unwrap_or(ptr::null_mut())
+        self.stream
     }
 
     pub fn as_mut_ptr(&mut self) -> cuda_sys::cudaStream {
-        self.stream.unwrap_or(ptr::null_mut())
+        self.stream
     }
 }
