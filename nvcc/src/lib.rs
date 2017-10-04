fe87256f194124f2e9b9b10d4423fc8a3ac10007
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-10-04 09:30:01 +0900

    add error messages

--- nvcc/src/lib.rs
+++ nvcc/src/lib.rs
@@ -1,32 +1,71 @@
 use std::env;
+use std::error::Error;
 use std::path;
 use std::process;
 
 #[cfg(unix)]
 pub fn compile_library(output: &str, files: &[&str]) {
-    let out_dir = path::PathBuf::from(&env::var("OUT_DIR").unwrap());
+    let out_dir = match env::var("OUT_DIR") {
+        Ok(out_dir) => path::PathBuf::from(out_dir),
+        Err(_) => {
+            eprintln!("nvcc: Cannot detect output directory.");
+            panic!()
+        }
+    };
 
     let mut objs = Vec::new();
     for file in files.iter() {
-        let name = path::Path::new(file).file_stem().unwrap();
+        let name = match path::Path::new(file).file_stem() {
+            Some(name) => name,
+            None => {
+                eprintln!("nvcc: Cannot detect stem for \"{}\".", file);
+                panic!()
+            }
+        };
         let mut obj = name.to_owned();
         obj.push(".o");
 
-        process::Command::new("nvcc")
+        let status = process::Command::new("nvcc")
             .args(&["-c", file, "-Xcompiler", "-fPIC", "-o"])
             .arg(&out_dir.join(&obj))
-            .status()
-            .unwrap();
+            .status();
+        match status {
+            Ok(status) => {
+                if status.success() {
+                    ()
+                } else {
+                    eprintln!("nvcc: \"nvcc\" exited with {}.", status);
+                    panic!()
+                }
+            }
+            Err(err) => {
+                eprintln!("nvcc: Cannot execute \"nvcc\". {}", err.description());
+                panic!()
+            }
+        }
 
         objs.push(obj);
     }
 
-    process::Command::new("ar")
+    let status = process::Command::new("ar")
         .args(&["crus", output])
         .args(objs.iter())
         .current_dir(&out_dir)
-        .status()
-        .unwrap();
+        .status();
+    match status {
+        Ok(status) => {
+            if status.success() {
+                ()
+            } else {
+                eprintln!("nvcc: \"ar\" exited with {}.", status);
+                panic!()
+            }
+        }
+        Err(err) => {
+            eprintln!("nvcc: Cannot execute \"ar\". {}", err.description());
+            panic!()
+        }
+    }
 
     println!("cargo:rustc-link-search=native={}", out_dir.display());
 }
