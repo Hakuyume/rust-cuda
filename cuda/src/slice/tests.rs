d9ae8da05697dcbceaa869957e1937edb543a7b9
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-10-06 00:33:58 +0900

    add tests

--- cuda/src/slice/tests.rs
+++ cuda/src/slice/tests.rs
@@ -20,6 +20,50 @@ fn from_raw_parts_mut() {
 }
 
 #[test]
+fn split_at() {
+    unsafe {
+        let s = super::from_raw_parts(32 as *const f32, 16);
+        let (l, r) = s.split_at(4);
+        assert_eq!(l.as_ptr(), 32 as *const f32);
+        assert_eq!(l.len(), 4);
+        assert_eq!(r.as_ptr(), (32 + mem::size_of::<f32>() * 4) as *const f32);
+        assert_eq!(r.len(), 16 - 4);
+    }
+}
+
+#[test]
+#[should_panic]
+fn split_at_invalid() {
+    unsafe {
+        let s = super::from_raw_parts(32 as *const f32, 16);
+        s.split_at(24);
+    }
+}
+
+#[test]
+fn split_at_mut() {
+    unsafe {
+        let s = super::from_raw_parts_mut(32 as *mut f32, 16);
+        let (l, r) = s.split_at_mut(4);
+        assert_eq!(l.as_ptr(), 32 as *const f32);
+        assert_eq!(l.as_mut_ptr(), 32 as *mut f32);
+        assert_eq!(l.len(), 4);
+        assert_eq!(r.as_ptr(), (32 + mem::size_of::<f32>() * 4) as *const f32);
+        assert_eq!(r.as_mut_ptr(), (32 + mem::size_of::<f32>() * 4) as *mut f32);
+        assert_eq!(r.len(), 16 - 4);
+    }
+}
+
+#[test]
+#[should_panic]
+fn split_at_invalid() {
+    unsafe {
+        let s = super::from_raw_parts_mut(32 as *mut f32, 16);
+        s.split_at_mut(24);
+    }
+}
+
+#[test]
 fn index_range_full() {
     unsafe {
         let s = super::from_raw_parts(32 as *const f32, 16);
