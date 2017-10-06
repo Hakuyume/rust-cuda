2db7d639850091fc1154b0a2356cf12a3e27d879
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-10-06 09:45:01 +0900

    add hook

--- cuda/src/memory/memory.rs
+++ cuda/src/memory/memory.rs
@@ -1,3 +1,4 @@
+use std::cell;
 use std::mem;
 use std::ops;
 use std::ptr;
@@ -8,6 +9,11 @@ use cuda_sys::{c_void, size_t};
 use Result;
 use slice;
 
+thread_local! {
+    static MALLOC_HOOK: cell::RefCell<Option<Box<Fn(*const (), usize)>>> = cell::RefCell::new(None);
+    static FREE_HOOK: cell::RefCell<Option<Box<Fn(*const (), usize)>>> = cell::RefCell::new(None);
+}
+
 pub struct Memory<T> {
     ptr: *mut T,
     len: usize,
@@ -17,6 +23,9 @@ impl<T> Memory<T> {
     pub fn new(len: usize) -> Result<Memory<T>> {
         let mut ptr = ptr::null_mut::<c_void>();
         unsafe { try_call!(cuda_sys::cudaMalloc(&mut ptr, (mem::size_of::<T>() * len) as size_t)) }
+        MALLOC_HOOK.with(|hook| if let Some(ref hook) = *hook.borrow() {
+                             hook(ptr as *const (), mem::size_of::<T>() * len);
+                         });
         Ok(Memory {
                ptr: ptr as *mut T,
                len,
@@ -27,6 +36,9 @@ impl<T> Memory<T> {
 impl<T> Drop for Memory<T> {
     fn drop(&mut self) {
         unsafe { cuda_sys::cudaFree(self.ptr as *mut c_void) };
+        FREE_HOOK.with(|hook| if let Some(ref hook) = *hook.borrow() {
+                           hook(self.ptr as *const (), mem::size_of::<T>() * self.len);
+                       });
     }
 }
 
@@ -41,4 +53,12 @@ impl<T> ops::DerefMut for Memory<T> {
     fn deref_mut(&mut self) -> &mut slice::Slice<T> {
         unsafe { slice::from_raw_parts_mut(self.ptr, self.len) }
     }
+}
+
+pub fn set_malloc_hook<F: 'static + Fn(*const (), usize)>(f: F) {
+    MALLOC_HOOK.with(|hook| *hook.borrow_mut() = Some(Box::new(f)));
+}
+
+pub fn set_free_hook<F: 'static + Fn(*const (), usize)>(f: F) {
+    FREE_HOOK.with(|hook| *hook.borrow_mut() = Some(Box::new(f)));
 }
--- cuda/src/memory/mod.rs
+++ cuda/src/memory/mod.rs
@@ -1,5 +1,7 @@
 mod memory;
 pub use self::memory::Memory;
+pub use self::memory::set_malloc_hook;
+pub use self::memory::set_free_hook;
 
 mod memcpy;
 pub use self::memcpy::memcpy;
