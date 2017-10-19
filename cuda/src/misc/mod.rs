481b4434d5f08a5ec7c4a0d16e9df5968fcb3407
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-10-19 11:27:43 +0900

    remove temp vec

--- cuda/src/misc/mod.rs
+++ cuda/src/misc/mod.rs
@@ -9,15 +9,8 @@ use stream;
 mod dim3;
 pub use self::dim3::Dim3;
 
-pub trait Arg {
-    fn as_void(&self) -> *const c_void;
-}
-
-impl<T: Sized> Arg for T {
-    fn as_void(&self) -> *const c_void {
-        self as *const T as *const c_void
-    }
-}
+pub trait Arg {}
+impl<T: Sized> Arg for T {}
 
 pub unsafe fn launch_kernel(func: *const c_void,
                             grid_dim: Dim3,
@@ -26,11 +19,10 @@ pub unsafe fn launch_kernel(func: *const c_void,
                             shared_mem: usize,
                             stream: Option<&mut stream::Stream>)
                             -> Result<()> {
-    let args: Vec<_> = args.iter().map(|arg| arg.as_void()).collect();
     try_call!(cuda_sys::cudaLaunchKernel(func,
                                          grid_dim.into(),
                                          block_dim.into(),
-                                         args.as_ptr(),
+                                         args.as_ptr() as *const *const c_void,
                                          shared_mem as size_t,
                                          match stream {
                                              Some(stream) => stream.as_mut_ptr(),
