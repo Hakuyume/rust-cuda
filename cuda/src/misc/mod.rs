a13c6c2b59e0c0c5ff93ff5e995619114abdb06d
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-10-19 16:31:22 +0900

    deref

--- cuda/src/misc/mod.rs
+++ cuda/src/misc/mod.rs
@@ -19,22 +19,24 @@ impl<T> Arg for T {
     }
 }
 
-pub unsafe fn launch_kernel(func: *const c_void,
-                            grid_dim: Dim3,
-                            block_dim: Dim3,
-                            args: &[&Arg],
-                            shared_mem: usize,
-                            stream: Option<&mut stream::Stream>)
-                            -> Result<()> {
-    let args: Vec<_> = args.iter().map(|arg| arg.as_void()).collect();
-    try_call!(cuda_sys::cudaLaunchKernel(func,
-                                         grid_dim.into(),
-                                         block_dim.into(),
-                                         args.as_ptr(),
-                                         shared_mem as size_t,
-                                         match stream {
-                                             Some(stream) => stream.as_mut_ptr(),
-                                             None => ptr::null_mut(),
-                                         }));
+pub fn launch_kernel(func: *const c_void,
+                     grid_dim: Dim3,
+                     block_dim: Dim3,
+                     args: &[&Arg],
+                     shared_mem: usize,
+                     stream: Option<&mut stream::Stream>)
+                     -> Result<()> {
+    let args: Vec<_> = args.iter().map(|arg| (*arg).as_void()).collect();
+    unsafe {
+        try_call!(cuda_sys::cudaLaunchKernel(func,
+                                             grid_dim.into(),
+                                             block_dim.into(),
+                                             args.as_ptr(),
+                                             shared_mem as size_t,
+                                             match stream {
+                                                 Some(stream) => stream.as_mut_ptr(),
+                                                 None => ptr::null_mut(),
+                                             }))
+    };
     Ok(())
 }
