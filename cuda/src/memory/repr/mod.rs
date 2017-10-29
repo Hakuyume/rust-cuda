af924ffcab538b6c4e617b4348259f85be103d5e
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-10-29 13:21:43 +0900

    add view

--- cuda/src/memory/repr/mod.rs
+++ cuda/src/memory/repr/mod.rs
@@ -5,6 +5,9 @@ pub trait Repr<T> {
     fn as_ptr(&self) -> *const T;
     fn len(&self) -> usize;
 
+    fn view<'a>(&'a self) -> View<'a, T> {
+        unsafe { from_raw_parts(self.as_ptr(), self.len()) }
+    }
     fn split_at<'a>(&'a self, mid: usize) -> (View<'a, T>, View<'a, T>) {
         assert!(mid <= self.len());
         unsafe {
@@ -17,9 +20,10 @@ pub trait Repr<T> {
 pub trait ReprMut<T>: Repr<T> {
     fn as_mut_ptr(&mut self) -> *mut T;
 
-    fn split_at_mut<'a>(&'a mut self,
-                        mid: usize)
-                        -> (ViewMut<'a, T>, ViewMut<'a, T>) {
+    fn view_mut<'a>(&'a mut self) -> ViewMut<'a, T> {
+        unsafe { from_raw_parts_mut(self.as_mut_ptr(), self.len()) }
+    }
+    fn split_at_mut<'a>(&'a mut self, mid: usize) -> (ViewMut<'a, T>, ViewMut<'a, T>) {
         assert!(mid <= self.len());
         unsafe {
             (from_raw_parts_mut(self.as_mut_ptr(), mid),
