87e8b018c76edee0ff0347124d17eae128018ab6
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-12-01 09:20:55 +0900

    add mut

--- cudnn/src/tensor/tests.rs
+++ cudnn/src/tensor/tests.rs
@@ -5,7 +5,7 @@ use super::Format;
 
 #[test]
 fn set_4d_nchw() {
-    let desc: Descriptor<f32> = Descriptor::new().unwrap();
+    let mut desc: Descriptor<f32> = Descriptor::new().unwrap();
     desc.set_4d(Format::NCHW, 2, 3, 5, 7).unwrap();
     assert_eq!(desc.len(), 2 * 3 * 5 * 7);
     assert_eq!(desc.get_size().unwrap(),
@@ -15,7 +15,7 @@ fn set_4d_nchw() {
 
 #[test]
 fn set_4d_nhwc() {
-    let desc: Descriptor<f32> = Descriptor::new().unwrap();
+    let mut desc: Descriptor<f32> = Descriptor::new().unwrap();
     desc.set_4d(Format::NHWC, 2, 3, 5, 7).unwrap();
     assert_eq!(desc.len(), 2 * 3 * 5 * 7);
     assert_eq!(desc.get_size().unwrap(),
@@ -25,7 +25,7 @@ fn set_4d_nhwc() {
 
 #[test]
 fn set_4d_ex() {
-    let desc: Descriptor<f32> = Descriptor::new().unwrap();
+    let mut desc: Descriptor<f32> = Descriptor::new().unwrap();
     desc.set_4d_ex(2, 3, 5, 7, 3 * 5 * 7 * 16, 5 * 7 * 8, 7 * 4, 2)
         .unwrap();
     assert_eq!(desc.len(), 2 * 3 * 5 * 7 * 16);
