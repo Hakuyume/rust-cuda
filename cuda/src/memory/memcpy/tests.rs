bcb27941b9a1f3a8c31d23eb27d9a014090f8fa4
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-12-03 23:47:23 +0900

    rename

--- cuda/src/memory/memcpy/tests.rs
+++ cuda/src/memory/memcpy/tests.rs
@@ -6,33 +6,31 @@ use super::Repr;
 
 #[test]
 fn memcpy() {
-    let host_src: Vec<f32> = {
+    let src: Vec<f32> = {
         let mut rng = rand::thread_rng();
         (0..256).map(|_| rng.gen()).collect()
     };
 
-    let mut device = Memory::new(host_src.len()).unwrap();
-    super::memcpy(&mut device, &host_src).unwrap();
+    let mut dev = Memory::new(src.len()).unwrap();
+    super::memcpy(&mut dev, &src).unwrap();
 
-    let mut host_dst = vec![0.; device.len()];
-    super::memcpy(&mut host_dst, &device).unwrap();
-    assert_eq!(&host_dst, &host_src);
+    let mut dst = vec![0.; dev.len()];
+    super::memcpy(&mut dst, &dev).unwrap();
+    assert_eq!(&dst, &src);
 }
 
 #[test]
 #[should_panic]
 fn memcpy_host_to_device_invalid() {
     let host = vec![0.; 128];
-    if let Ok(mut device) = Memory::new(256) {
-        super::memcpy(&mut device, &host).unwrap();
-    }
+    let mut dev = Memory::new(256).unwrap();
+    super::memcpy(&mut dev, &host).unwrap();
 }
 
 #[test]
 #[should_panic]
 fn memcpy_device_to_host_invalid() {
     let mut host = vec![0.; 256];
-    if let Ok(device) = Memory::new(128) {
-        super::memcpy(&mut host, &device).unwrap();
-    }
+    let dev = Memory::new(128).unwrap();
+    super::memcpy(&mut host, &dev).unwrap();
 }
