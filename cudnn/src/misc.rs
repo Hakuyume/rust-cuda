9ce6252b3ea242ff3d0b19a94b57de96037c6d3f
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-12-07 23:57:33 +0900

    add generic param

--- cudnn/src/misc.rs
+++ cudnn/src/misc.rs
@@ -7,30 +7,27 @@ use Result;
 use tensor;
 use filter;
 
-pub trait MemoryDescriptor {
-    type T;
+pub trait MemoryDescriptor<T> {
     fn get_size(&self) -> Result<usize>;
     fn check_memory<R>(&self, mem: &R) -> Result<()>
-        where R: Repr<Self::T>
+        where R: Repr<T>
     {
-        assert_eq!(self.get_size()?, mem::size_of::<Self::T>() * mem.len());
+        assert_eq!(self.get_size()?, mem::size_of::<T>() * mem.len());
         Ok(())
     }
 }
 
-impl<T> MemoryDescriptor for tensor::Descriptor<T>
+impl<T> MemoryDescriptor<T> for tensor::Descriptor<T>
     where T: scalar::Scalar
 {
-    type T = T;
     fn get_size(&self) -> Result<usize> {
         self.get_size_in_bytes()
     }
 }
 
-impl<T> MemoryDescriptor for filter::Descriptor<T>
+impl<T> MemoryDescriptor<T> for filter::Descriptor<T>
     where T: scalar::Scalar
 {
-    type T = T;
     fn get_size(&self) -> Result<usize> {
         let (_, k, c, h, w) = self.get_4d()?;
         Ok((mem::size_of::<T>() * k * c * h * w))
