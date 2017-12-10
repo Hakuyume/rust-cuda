e3f4165c260883173a556c42fc504b462b5967ed
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-12-10 20:24:45 +0900

    use std::any

--- cuda/src/misc/mod.rs
+++ cuda/src/misc/mod.rs
@@ -1,3 +1,4 @@
+use std::any;
 use std::ptr;
 
 use cuda_sys;
@@ -9,18 +10,15 @@ use stream;
 mod dim3;
 pub use self::dim3::Dim3;
 
-pub trait Arg {}
-impl<T> Arg for T {}
-
 pub unsafe fn launch_kernel(func: *const c_void,
                             grid_dim: Dim3,
                             block_dim: Dim3,
-                            args: &[&Arg],
+                            args: &[&any::Any],
                             shared_mem: usize,
                             stream: Option<&mut stream::Stream>)
                             -> Result<()> {
     let args: Vec<_> = args.iter()
-        .map(|arg| *arg as *const Arg as *const c_void)
+        .map(|arg| *arg as *const any::Any as *const c_void)
         .collect();
     try_call!(cuda_sys::cudaLaunchKernel(func,
                                          grid_dim.into(),
