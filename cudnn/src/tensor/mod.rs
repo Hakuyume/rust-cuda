e46fdd266a55da0d6b3b12f2897430d80672633e
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-12-12 23:56:55 +0900

    add tensor::add

--- cudnn/src/tensor/mod.rs
+++ cudnn/src/tensor/mod.rs
@@ -1,6 +1,6 @@
 use std::os::raw::c_void;
 
-use cuda::memory::ReprMut;
+use cuda::memory::{Repr, ReprMut};
 use cudnn_sys;
 
 use Result;
@@ -14,6 +14,30 @@ mod descriptor;
 pub use self::descriptor::Descriptor;
 
 use misc::MemoryDescriptor;
+
+pub fn add<T, S, A, C>(context: &mut context::Context,
+                       alpha: S,
+                       a: (&Descriptor<T>, &A),
+                       beta: S,
+                       c: (&Descriptor<T>, &mut C))
+                       -> Result<()>
+    where T: scalar::Scalar + scalar::Scale<Scale = S>,
+          A: Repr<T>,
+          C: ReprMut<T>
+{
+    a.0.check_memory(a.1)?;
+    c.0.check_memory(c.1)?;
+    unsafe {
+        try_call!(cudnn_sys::cudnnAddTensor(context.as_mut_ptr(),
+                                            &alpha as *const S as *const c_void,
+                                            a.0.as_ptr(),
+                                            a.1.as_ptr() as *const c_void,
+                                            &beta as *const S as *const c_void,
+                                            c.0.as_ptr(),
+                                            c.1.as_mut_ptr() as *mut c_void))
+    }
+    Ok(())
+}
 
 pub fn scale<T, S, Y>(context: &mut context::Context,
                       y: (&Descriptor<T>, &mut Y),
