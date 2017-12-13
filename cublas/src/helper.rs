c0709f659ce5f30c55547dc1c1df204fbf1e5bcf
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-12-13 11:09:57 +0900

    add check_mat

--- cublas/src/helper.rs
+++ cublas/src/helper.rs
@@ -3,5 +3,12 @@ use cuda::memory::Repr;
 pub fn check_vec<T, X>(n: usize, x: &X, incx: usize)
     where X: Repr<T>
 {
-    assert!((n - 1) * incx + 1 <= x.len());
+    assert!(1 + (n - 1) * incx <= x.len());
+}
+
+pub fn check_mat<T, X>(m: usize, n: usize, a: &X, lda: usize)
+    where X: Repr<T>
+{
+    assert!(m <= lda);
+    assert!(lda * n <= a.len());
 }
