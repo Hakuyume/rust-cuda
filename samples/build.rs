99f28c10d9a1108ad25e29e34f9b4d0639628af1
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-12-16 17:54:53 +0900

    rename files

--- samples/build.rs
+++ samples/build.rs
@@ -1,5 +1,5 @@
 extern crate nvcc;
 
 fn main() {
-    nvcc::compile_library("libkernel.a", &["src/bin/vector_add.cu"]);
+    nvcc::compile_library("libkernel.a", &["src/bin/vector_add/vectorAdd.cu"]);
 }
